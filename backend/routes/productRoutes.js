a0228022193ef90af245e10b4686e583551e5f59
mishti <msihti8112gmail.com>
2025-08-25 11:47:04 +0530

    update product route for images

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {protect, authorize} = equire("../middleware/auth");
+const multer = require("multer");
 
 const{
     createProduct,
@@ -8,7 +9,19 @@ const{
     updateProduct,
     deleteProduct
 } = require("../controllers/productController");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/"); // folder must exist
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
 router.get("/", getProducts);
-router.post("/", protect, authorize("seller"), createProduct);
+router.post("/", protect, authorize("seller"), upload.array("images", 5), createProduct); 
 router.put("/:id", protect, authorize("seller"), updateProduct);
 router.delete("/:id", protect, authorize("seller"), deleteProduct);
