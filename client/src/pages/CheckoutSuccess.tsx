07e5af3b6b97657671b2c03cd481b808a0eb7d7c
Mishti Garg <msihti811@gmail.com>
2025-09-04 19:31:54 +0530

    Update CheckoutSuccess.tsx

--- client/src/pages/CheckoutSuccess.tsx
+++ client/src/pages/CheckoutSuccess.tsx
@@ -23,7 +23,7 @@ export default function CheckoutSuccess() {
       }
     };
     completeOrder();
-  }, []); // Empty dependency array
+  }, []); 
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-background px-4">
